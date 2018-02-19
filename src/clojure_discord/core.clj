e4a135e7719515559a9d7fa5329f1005ce2d27d0
Will Curry <watercrafthd@gmail.com>
2018-02-19 23:00:28 +0000

    refactor code to use new get-request function

--- src/clojure_discord/core.clj
+++ src/clojure_discord/core.clj
@@ -6,20 +6,25 @@
 (def ^:private ^:const base-url "https://discordapp.com/api/v6/")
 (def ^:private ^:const token (:token (read-config "config.edn")))
 
-(defn -main
-  [& args])
+(def ^:private get-requests
+  {:pinned-messages "channels/?/pins"
+   :channel "channels/?"})
 
 (defn- add-base-url [end-url]
   (str base-url end-url))
+
+(defn- create-request [request args]
+  (cond
+    (not (clojure.string/includes? request "?")) (add-base-url request)
+    :else
+      (recur (clojure.string/replace request "?" (first args)) (rest args))))
 
 (defn get-request [url]
   (json/read-str
     (:body (client/get url {:headers {"Authorization" (str "Bot " token)}}))))
 
 (defn get-pinned-messages [channel-id]
-  (let [end-url (str "channels/" channel-id "/pins")]
-    (get-request (add-base-url end-url))))
+  (get-request (create-request (:pinned-messages get-requests) [channel-id])))
 
 (defn get-channel [channel-id]
-  (let [end-url (str "channels/" channel-id)]
-    (get-request (add-base-url end-url))))
+  (get-request (create-request (:channel get-requests) [channel-id])))
