9f4f25b217e6bd019d6bb870fee63b2a17d92cc6
Will Curry <watercrafthd@gmail.com>
2018-02-19 17:11:39 +0000

    initial

---
+++ spec/clojure_discord/core_spec.clj
@@ -0,0 +1,7 @@
+(ns clojure-discord.core-spec
+  (:require [speclj.core :refer :all]
+            [clojure-discord.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/clojure_discord/core.clj
@@ -0,0 +1,5 @@
+(ns clojure-discord.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
