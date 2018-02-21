ce08ba1f95c8640c5920715859e65282d9cded5b
Will Curry <watercrafthd@gmail.com>
2018-02-21 13:29:48 +0000

    Add test to ensure event/handle is called

--- spec/clojure_discord/gateway_spec.clj
+++ spec/clojure_discord/gateway_spec.clj
@@ -1,7 +1,9 @@
 (ns clojure-discord.gateway-spec
   (:require [speclj.core :refer :all]
+            [clojure.data.json :as json]
             [clojure-discord.gateway :as gateway]
             [clojure-discord.parser :as parser]
+            [clojure-discord.events :as events]
             [clojure-discord.requests :as request]
             [clojure-discord.socket :as socket]))
 
@@ -34,4 +36,20 @@
                                #'socket/set-handler-function (fn [function] (reset! handler-function function))
                                #'parser/parse (stub :parse)}
                                (fn [] (gateway/connect) (@handler-function "fake-payload")))
-                  (should-have-invoked :parse))))
+                  (should-have-invoked :parse)))
+
+          (def fake-json-payload
+            (json/write-str {:op 0
+                 :d "data"
+                 :s 1
+                 :t "READY"}))
+
+          (it "handler function calls events/handle when op code is 0"
+              (let [handler-function (atom nil)]
+              (with-redefs-fn {#'socket/create-connection (stub :create-connection)
+                               #'socket/identify-with-discord (stub :identify)
+                               #'request/get (stub :get-request)
+                               #'events/handle (stub :event-handle)
+                               #'socket/set-handler-function (fn [function] (reset! handler-function function))}
+                               (fn [] (gateway/connect) (@handler-function fake-json-payload)))
+                  (should-have-invoked :event-handle))))
